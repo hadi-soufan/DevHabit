8ac47343d8e881b18e7862ce026090252bfd72e6
hadi-soufan <hady.a.soufan@gmail.com>
2025-04-06 12:11:47 +0300

    DB: getting started with db

---
+++ src/DevHabit.Api/Database/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DevHabit.Api.Database;
+
+public sealed class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.HasDefaultSchema(Schemas.Application);
+    }
+}
