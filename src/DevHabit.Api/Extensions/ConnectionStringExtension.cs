548a2d808c2c8576f53d5f80d0e8e47e8750232c
hadi-soufan <hady.a.soufan@gmail.com>
2025-04-06 12:12:20 +0300

    INFRA: added db extensions

---
+++ src/DevHabit.Api/Extensions/ConnectionStringExtension.cs
@@ -0,0 +1,23 @@
+using DevHabit.Api.Database;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql;
+
+namespace DevHabit.Api.Extensions
+{
+    public static class ConnectionStringExtension
+    {
+        public static IServiceCollection AddCustomDbContext(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options
+                    .UseNpgsql(
+                        configuration.GetConnectionString("Database"),
+                        npgsqlOptions => npgsqlOptions
+                            .MigrationsHistoryTable(HistoryRepository.DefaultTableName, Schemas.Application))
+                    .UseSnakeCaseNamingConvention());
+
+            return services;
+        }
+    }
+}
